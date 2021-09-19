a959cfcc03bc581879476c7fd99748f013b17dc0
Daniel Armas <daniel.armas9706@gmail.com>
2021-09-19 10:38:49 -0400

    - Improving the UserRespository.

--- lib/domain/repositories/user_repository.dart
+++ lib/domain/repositories/user_repository.dart
@@ -1,13 +1,23 @@
 import 'package:dartz/dartz.dart';
 import 'package:grpc/grpc.dart';
+import 'package:postgres/postgres.dart';
 
 import '../../protos/protos/main.pb.dart';
 
-// ignore: one_member_abstracts
 abstract class UserRepository {
   Future<Either<GrpcError, User>> createUser(
-      {required Map<String, dynamic> data});
-  Future<Either<GrpcError, Iterable<User>>> listUser();
-  Future<Either<GrpcError, User>> getUser({required String id});
-  Future<Either<GrpcError, void>> deleteUser({required String id});
+      {required PostgreSQLExecutionContext context,
+      required Map<String, dynamic> data,
+      required List<String> paths});
+  Future<Either<GrpcError, Iterable<User>>> listUser(
+      {required PostgreSQLExecutionContext context,
+      required Map<String, dynamic> data,
+      required List<String> paths});
+  Future<Either<GrpcError, User>> getUser(
+      {required PostgreSQLExecutionContext context,
+      required Map<String, dynamic> data,
+      required List<String> paths});
+  Future<Either<GrpcError, void>> deleteUser(
+      {required PostgreSQLExecutionContext context,
+      required Map<String, dynamic> data});
 }
