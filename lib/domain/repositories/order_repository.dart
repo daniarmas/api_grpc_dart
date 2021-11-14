f81fc2a3cabc2f2c5af7f6cfa8a400afc41eb850
Daniel Armas <daniel.armas9706@gmail.com>
2021-11-14 08:57:41 -0500

    Fixing the order_repository.

--- lib/domain/repositories/order_repository.dart
+++ lib/domain/repositories/order_repository.dart
@@ -9,19 +9,13 @@ import 'package:api_grpc_dart/core/utils/metadata.dart';
 import '../../protos/protos/main.pb.dart' as grpc_model;
 
 abstract class OrderRepository {
-  Future<Either<GrpcError, List<grpc_model.Order>>> listItem({
+  Future<Either<GrpcError, List<grpc_model.Order>>> listOrder({
     required PostgreSQLExecutionContext context,
     required Map<String, dynamic> data,
     required HeadersMetadata metadata,
     required List<Attribute> paths,
   });
-  Future<Either<GrpcError, grpc_model.SearchItemResponse>> searchItem({
-    required PostgreSQLExecutionContext context,
-    required Map<String, dynamic> data,
-    required HeadersMetadata metadata,
-    required List<Attribute> paths,
-  });
-  Future<Either<GrpcError, grpc_model.Item>> getItem({
+  Future<Either<GrpcError, grpc_model.Order>> getOrder({
     required PostgreSQLExecutionContext context,
     required Map<String, dynamic> data,
     required HeadersMetadata metadata,
