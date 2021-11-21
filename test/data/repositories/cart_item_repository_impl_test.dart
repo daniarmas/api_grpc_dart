28f2a9e7b968cd9ea582e7c4413db5bccec0e7b8
Daniel Armas <daniel.armas9706@gmail.com>
2021-11-21 10:59:08 -0500

    Implementing the unit test for ListCartItem.

--- test/data/repositories/cart_item_repository_impl_test.dart
+++ test/data/repositories/cart_item_repository_impl_test.dart
@@ -8,25 +8,284 @@ import 'package:postgres/postgres.dart';
 import 'package:test/test.dart';
 
 // Project imports:
+import 'package:api_grpc_dart/core/utils/json_web_token.dart';
 import 'package:api_grpc_dart/core/utils/metadata.dart';
 import 'package:api_grpc_dart/data/database/database.dart';
+import 'package:api_grpc_dart/data/datasources/authorization_token_local_data_source.dart';
 import 'package:api_grpc_dart/data/datasources/cart_item_local_data_source.dart';
 import 'package:api_grpc_dart/data/repositories/cart_item_repository_impl.dart';
 import 'package:api_grpc_dart/environment.dart';
 import 'package:api_grpc_dart/injection_container.dart';
 import 'package:api_grpc_dart/protos/protos/main.pb.dart';
-import 'item_repository_impl_test.mocks.dart';
+import './cart_item_repository_impl_test.mocks.dart';
 
-@GenerateMocks([CartItemLocalDataSource, Database])
+@GenerateMocks([
+  CartItemLocalDataSource,
+  Database,
+  AuthorizationTokenLocalDataSource,
+  JsonWebToken,
+])
 void main() {
   late EnvironmentApp environment;
-  // late MockCartItemLocalDataSource mockCartItemLocalDataSource;
+  late MockCartItemLocalDataSource mockCartItemLocalDataSource;
   late MockDatabase mockDatabase;
+  late MockJsonWebToken mockJsonWebToken;
+  late MockAuthorizationTokenLocalDataSource
+      mockAuthorizationTokenLocalDataSource;
   late CartItemRepositoryImpl cartItemRepositoryImpl;
   late PostgreSQLConnection connection;
   late PostgreSQLExecutionContext ctx;
   late HeadersMetadata metadata;
-  test('cart item repository impl ...', () async {
-    // TODO: Implement test
+
+  setUpAll(() {
+    configureDependencies();
+    environment = GetIt.I<EnvironmentApp>();
+  });
+
+  setUp(() async {
+    connection = PostgreSQLConnection(environment.databaseHost,
+        environment.databasePort, environment.databaseDatabase,
+        username: environment.databaseUsername,
+        password: environment.databasePassword);
+    await connection.open();
+    await connection.transaction((context) async {
+      ctx = context;
+    });
+    metadata = HeadersMetadata(
+        authorization: '1',
+        systemVersionSdk: '1',
+        accesstoken: '1',
+        platform: PlatformType.Android,
+        systemVersion: '1',
+        appVersion: '1',
+        networkType: 'wifi',
+        systemLanguage: 'en',
+        ipv4: '192.168.1.3',
+        ipv6: 'asdksajdhaskjdjklasdhjk',
+        app: AppType.App,
+        deviceId: '1',
+        model: '1',
+        firebaseCloudMessagingId: '1');
+    mockDatabase = MockDatabase();
+    mockJsonWebToken = MockJsonWebToken();
+    mockAuthorizationTokenLocalDataSource =
+        MockAuthorizationTokenLocalDataSource();
+    mockCartItemLocalDataSource = MockCartItemLocalDataSource();
+    cartItemRepositoryImpl = CartItemRepositoryImpl(
+      database: mockDatabase,
+      cartItemLocalDataSource: mockCartItemLocalDataSource,
+      authorizationTokenLocalDataSource: mockAuthorizationTokenLocalDataSource,
+      jsonWebToken: mockJsonWebToken,
+    );
+  });
+
+  group('testing listCartItem', () {
+    test('Return data sucessfull when everything is ok', () async {
+      // setup
+      Map<String, dynamic> data = {
+        'nextPage': '2',
+      };
+      AuthorizationToken authorizationToken = AuthorizationToken(
+        id: '1',
+        app: AppType.App,
+        appVersion: '1',
+        createTime: '1',
+        deviceFk: '1',
+        refreshTokenFk: '1',
+        updateTime: '1',
+        userFk: '1',
+        valid: true,
+      );
+      List<Item> listOfCartItem = [
+        Item(
+          id: '1',
+          availability: 1,
+          businessFk: 'businessFk',
+          businessItemCategoryFk: 'businessItemCategoryFk',
+          description: 'description',
+          status: ItemStatusType.Available,
+          name: 'name',
+          photos: [
+            ItemPhoto(
+                createTime: 'createTime',
+                highQualityPhoto: 'highQualityPhoto',
+                id: 'id',
+                itemFk: 'itemFk',
+                lowQualityPhoto: 'lowQualityPhoto',
+                updateTime: 'updateTime')
+          ],
+          price: 20.0,
+          createTime: '1',
+          updateTime: '1',
+        ),
+        Item(
+          id: '1',
+          availability: 1,
+          businessFk: 'businessFk',
+          businessItemCategoryFk: 'businessItemCategoryFk',
+          description: 'description',
+          status: ItemStatusType.Available,
+          name: 'name',
+          photos: [
+            ItemPhoto(
+                createTime: 'createTime',
+                highQualityPhoto: 'highQualityPhoto',
+                id: 'id',
+                itemFk: 'itemFk',
+                lowQualityPhoto: 'lowQualityPhoto',
+                updateTime: 'updateTime')
+          ],
+          price: 20.0,
+          createTime: '1',
+          updateTime: '1',
+        ),
+      ];
+      // side effects
+      when(mockJsonWebToken.verify(
+              metadata.authorization!, 'AuthorizationToken'))
+          .thenAnswer((_) => {
+                'authorizationTokenFk': metadata.authorization!,
+              });
+      when(mockAuthorizationTokenLocalDataSource.getAuthorizationToken(
+        context: anyNamed('context'),
+        data: anyNamed('data'),
+        paths: anyNamed('paths'),
+      )).thenAnswer((_) async => authorizationToken);
+      when(mockCartItemLocalDataSource.listCartItem(
+              context: anyNamed('context'),
+              data: anyNamed('data'),
+              paths: anyNamed('paths')))
+          .thenAnswer((_) async => listOfCartItem);
+      final result = await cartItemRepositoryImpl.listCartItem(
+          context: ctx, data: data, metadata: metadata, paths: []);
+      // expectations
+      verify(mockJsonWebToken.verify(
+          metadata.authorization!, 'AuthorizationToken'));
+      verify(mockAuthorizationTokenLocalDataSource.getAuthorizationToken(
+        context: anyNamed('context'),
+        data: anyNamed('data'),
+        paths: anyNamed('paths'),
+      ));
+      verify(mockCartItemLocalDataSource.listCartItem(
+          context: anyNamed('context'),
+          data: anyNamed('data'),
+          paths: anyNamed('paths')));
+      expect(result, Right(listOfCartItem));
+    });
+    test(
+        'Return GrpcError.unauthenticated when the AuthorizationToken not exists',
+        () async {
+      // setup
+      Map<String, dynamic> data = {
+        'nextPage': '2',
+      };
+      // side effects
+      when(mockJsonWebToken.verify(
+              metadata.authorization!, 'AuthorizationToken'))
+          .thenAnswer((_) => {
+                'authorizationTokenFk': metadata.authorization!,
+              });
+      when(mockAuthorizationTokenLocalDataSource.getAuthorizationToken(
+        context: anyNamed('context'),
+        data: anyNamed('data'),
+        paths: anyNamed('paths'),
+      )).thenAnswer((_) async => null);
+      final result = await cartItemRepositoryImpl.listCartItem(
+          context: ctx, data: data, metadata: metadata, paths: []);
+      // expectations
+      verify(mockJsonWebToken.verify(
+          metadata.authorization!, 'AuthorizationToken'));
+      verify(mockAuthorizationTokenLocalDataSource.getAuthorizationToken(
+        context: anyNamed('context'),
+        data: anyNamed('data'),
+        paths: anyNamed('paths'),
+      ));
+      verifyNever(mockCartItemLocalDataSource.listCartItem(
+          context: anyNamed('context'),
+          data: anyNamed('data'),
+          paths: anyNamed('paths')));
+      expect(result, Left(GrpcError.unauthenticated('Unauthenticated')));
+    });
+    test(
+        'Return GrpcError.unauthenticated when the authorizationToken has expired',
+        () async {
+      // setup
+      Map<String, dynamic> data = {
+        'nextPage': '2',
+      };
+      // side effects
+      when(mockJsonWebToken.verify(
+              metadata.authorization!, 'AuthorizationToken'))
+          .thenThrow(GrpcError.unauthenticated('AuthorizationToken expired'));
+      when(mockAuthorizationTokenLocalDataSource.getAuthorizationToken(
+        context: anyNamed('context'),
+        data: anyNamed('data'),
+        paths: anyNamed('paths'),
+      )).thenAnswer((_) async => null);
+      final result = await cartItemRepositoryImpl.listCartItem(
+          context: ctx, data: data, metadata: metadata, paths: []);
+      // expectations
+      verify(mockJsonWebToken.verify(
+          metadata.authorization!, 'AuthorizationToken'));
+      verifyNever(mockAuthorizationTokenLocalDataSource.getAuthorizationToken(
+        context: anyNamed('context'),
+        data: anyNamed('data'),
+        paths: anyNamed('paths'),
+      ));
+      verifyNever(mockCartItemLocalDataSource.listCartItem(
+          context: anyNamed('context'),
+          data: anyNamed('data'),
+          paths: anyNamed('paths')));
+      expect(result,
+          Left(GrpcError.unauthenticated('AuthorizationToken expired')));
+    });
+    test('Return GrpcError.internal when the code throw a Exception', () async {
+      // setup
+      Map<String, dynamic> data = {
+        'nextPage': '2',
+      };
+      AuthorizationToken authorizationToken = AuthorizationToken(
+        id: '1',
+        app: AppType.App,
+        appVersion: '1',
+        createTime: '1',
+        deviceFk: '1',
+        refreshTokenFk: '1',
+        updateTime: '1',
+        userFk: '1',
+        valid: true,
+      );
+      // side effects
+      when(mockJsonWebToken.verify(
+              metadata.authorization!, 'AuthorizationToken'))
+          .thenAnswer((_) => {
+                'authorizationTokenFk': metadata.authorization!,
+              });
+      when(mockAuthorizationTokenLocalDataSource.getAuthorizationToken(
+        context: anyNamed('context'),
+        data: anyNamed('data'),
+        paths: anyNamed('paths'),
+      )).thenAnswer((_) async => authorizationToken);
+      when(mockCartItemLocalDataSource.listCartItem(
+              context: anyNamed('context'),
+              data: anyNamed('data'),
+              paths: anyNamed('paths')))
+          .thenThrow(Exception());
+      final result = await cartItemRepositoryImpl.listCartItem(
+          context: ctx, data: data, metadata: metadata, paths: []);
+      // expectations
+      verify(mockJsonWebToken.verify(
+          metadata.authorization!, 'AuthorizationToken'));
+      verify(mockAuthorizationTokenLocalDataSource.getAuthorizationToken(
+        context: anyNamed('context'),
+        data: anyNamed('data'),
+        paths: anyNamed('paths'),
+      ));
+      verify(mockCartItemLocalDataSource.listCartItem(
+          context: anyNamed('context'),
+          data: anyNamed('data'),
+          paths: anyNamed('paths')));
+      expect(result, Left(GrpcError.internal('Internal server error')));
+    });
   });
 }
--- test/data/repositories/cart_item_repository_impl_test.mocks.dart
+++ test/data/repositories/cart_item_repository_impl_test.mocks.dart
@@ -3,7 +3,7 @@
 // Do not manually edit this file.
 
 // Dart imports:
-import 'dart:async' as _i4;
+import 'dart:async' as _i5;
 
 // Package imports:
 import 'package:mockito/mockito.dart' as _i1;
@@ -11,11 +11,14 @@ import 'package:postgres/postgres.dart' as _i2;
 import 'package:postgres_conector/postgres_conector.dart' as _i6;
 
 // Project imports:
+import 'package:api_grpc_dart/core/utils/json_web_token.dart' as _i9;
 import 'package:api_grpc_dart/data/database/database.dart' as _i7;
-import 'package:api_grpc_dart/protos/protos/main.pb.dart' as _i5;
+import 'package:api_grpc_dart/protos/protos/main.pb.dart' as _i3;
 
+import 'package:api_grpc_dart/data/datasources/authorization_token_local_data_source.dart'
+    as _i8;
 import 'package:api_grpc_dart/data/datasources/cart_item_local_data_source.dart'
-    as _i3;
+    as _i4;
 
 // ignore_for_file: avoid_redundant_argument_values
 // ignore_for_file: avoid_setters_without_getters
@@ -28,34 +31,37 @@ import 'package:api_grpc_dart/data/datasources/cart_item_local_data_source.dart'
 class _FakePostgreSQLConnection_0 extends _i1.Fake
     implements _i2.PostgreSQLConnection {}
 
+class _FakeAuthorizationToken_1 extends _i1.Fake
+    implements _i3.AuthorizationToken {}
+
 /// A class which mocks [CartItemLocalDataSource].
 ///
 /// See the documentation for Mockito's code generation for more information.
 class MockCartItemLocalDataSource extends _i1.Mock
-    implements _i3.CartItemLocalDataSource {
+    implements _i4.CartItemLocalDataSource {
   MockCartItemLocalDataSource() {
     _i1.throwOnMissingStub(this);
   }
 
   @override
-  _i4.Future<List<_i5.Item>> listCartItem(
+  _i5.Future<List<_i3.Item>> listCartItem(
           {_i2.PostgreSQLExecutionContext? context,
           Map<String, dynamic>? data,
           List<_i6.Attribute>? paths}) =>
       (super.noSuchMethod(
               Invocation.method(#listCartItem, [],
                   {#context: context, #data: data, #paths: paths}),
-              returnValue: Future<List<_i5.Item>>.value(<_i5.Item>[]))
-          as _i4.Future<List<_i5.Item>>);
+              returnValue: Future<List<_i3.Item>>.value(<_i3.Item>[]))
+          as _i5.Future<List<_i3.Item>>);
   @override
-  _i4.Future<_i5.Item?> getCartItem(
+  _i5.Future<_i3.Item?> getCartItem(
           {_i2.PostgreSQLExecutionContext? context,
           Map<String, dynamic>? data,
           List<_i6.Attribute>? paths}) =>
       (super.noSuchMethod(
           Invocation.method(#getCartItem, [],
               {#context: context, #data: data, #paths: paths}),
-          returnValue: Future<_i5.Item?>.value()) as _i4.Future<_i5.Item?>);
+          returnValue: Future<_i3.Item?>.value()) as _i5.Future<_i3.Item?>);
   @override
   String toString() => super.toString();
 }
@@ -69,20 +75,20 @@ class MockDatabase extends _i1.Mock implements _i7.Database {
   }
 
   @override
-  _i4.Future<bool> connect() =>
+  _i5.Future<bool> connect() =>
       (super.noSuchMethod(Invocation.method(#connect, []),
-          returnValue: Future<bool>.value(false)) as _i4.Future<bool>);
+          returnValue: Future<bool>.value(false)) as _i5.Future<bool>);
   @override
   void close() => super.noSuchMethod(Invocation.method(#close, []),
       returnValueForMissingStub: null);
   @override
-  _i4.Future<_i2.PostgreSQLConnection> getConnection() =>
+  _i5.Future<_i2.PostgreSQLConnection> getConnection() =>
       (super.noSuchMethod(Invocation.method(#getConnection, []),
               returnValue: Future<_i2.PostgreSQLConnection>.value(
                   _FakePostgreSQLConnection_0()))
-          as _i4.Future<_i2.PostgreSQLConnection>);
+          as _i5.Future<_i2.PostgreSQLConnection>);
   @override
-  _i4.Future<List<Map<String, dynamic>>> list(
+  _i5.Future<List<Map<String, dynamic>>> list(
           {_i2.PostgreSQLExecutionContext? context,
           String? table,
           List<_i6.Attribute>? attributes,
@@ -104,9 +110,9 @@ class MockDatabase extends _i1.Mock implements _i7.Database {
               }),
               returnValue: Future<List<Map<String, dynamic>>>.value(
                   <Map<String, dynamic>>[]))
-          as _i4.Future<List<Map<String, dynamic>>>);
+          as _i5.Future<List<Map<String, dynamic>>>);
   @override
-  _i4.Future<List<Map<String, dynamic>>> search(
+  _i5.Future<List<Map<String, dynamic>>> search(
           {_i2.PostgreSQLExecutionContext? context,
           String? table,
           List<_i6.Where>? where,
@@ -128,9 +134,9 @@ class MockDatabase extends _i1.Mock implements _i7.Database {
               }),
               returnValue: Future<List<Map<String, dynamic>>>.value(
                   <Map<String, dynamic>>[]))
-          as _i4.Future<List<Map<String, dynamic>>>);
+          as _i5.Future<List<Map<String, dynamic>>>);
   @override
-  _i4.Future<Map<String, dynamic>?> get(
+  _i5.Future<Map<String, dynamic>?> get(
           {_i2.PostgreSQLExecutionContext? context,
           String? table,
           List<_i6.Attribute>? attributes,
@@ -147,9 +153,9 @@ class MockDatabase extends _i1.Mock implements _i7.Database {
                 #agregationMethods: agregationMethods
               }),
               returnValue: Future<Map<String, dynamic>?>.value())
-          as _i4.Future<Map<String, dynamic>?>);
+          as _i5.Future<Map<String, dynamic>?>);
   @override
-  _i4.Future<Map<String, dynamic>> create(
+  _i5.Future<Map<String, dynamic>> create(
           {_i2.PostgreSQLExecutionContext? context,
           String? table,
           Map<String, dynamic>? data,
@@ -163,9 +169,9 @@ class MockDatabase extends _i1.Mock implements _i7.Database {
               }),
               returnValue:
                   Future<Map<String, dynamic>>.value(<String, dynamic>{}))
-          as _i4.Future<Map<String, dynamic>>);
+          as _i5.Future<Map<String, dynamic>>);
   @override
-  _i4.Future<Map<String, dynamic>?> update(
+  _i5.Future<Map<String, dynamic>?> update(
           {_i2.PostgreSQLExecutionContext? context,
           String? table,
           Map<String, dynamic>? data,
@@ -180,16 +186,96 @@ class MockDatabase extends _i1.Mock implements _i7.Database {
                 #attributes: attributes
               }),
               returnValue: Future<Map<String, dynamic>?>.value())
-          as _i4.Future<Map<String, dynamic>?>);
+          as _i5.Future<Map<String, dynamic>?>);
   @override
-  _i4.Future<bool> delete(
+  _i5.Future<bool> delete(
           {_i2.PostgreSQLExecutionContext? context,
           String? table,
           List<_i6.Where>? where}) =>
       (super.noSuchMethod(
           Invocation.method(
               #delete, [], {#context: context, #table: table, #where: where}),
-          returnValue: Future<bool>.value(false)) as _i4.Future<bool>);
+          returnValue: Future<bool>.value(false)) as _i5.Future<bool>);
+  @override
+  String toString() => super.toString();
+}
+
+/// A class which mocks [AuthorizationTokenLocalDataSource].
+///
+/// See the documentation for Mockito's code generation for more information.
+class MockAuthorizationTokenLocalDataSource extends _i1.Mock
+    implements _i8.AuthorizationTokenLocalDataSource {
+  MockAuthorizationTokenLocalDataSource() {
+    _i1.throwOnMissingStub(this);
+  }
+
+  @override
+  _i5.Future<_i3.AuthorizationToken> createAuthorizationToken(
+          {_i2.PostgreSQLExecutionContext? context,
+          Map<String, dynamic>? data,
+          List<_i6.Attribute>? paths}) =>
+      (super.noSuchMethod(
+              Invocation.method(#createAuthorizationToken, [],
+                  {#context: context, #data: data, #paths: paths}),
+              returnValue: Future<_i3.AuthorizationToken>.value(
+                  _FakeAuthorizationToken_1()))
+          as _i5.Future<_i3.AuthorizationToken>);
+  @override
+  _i5.Future<List<_i3.AuthorizationToken>> listAuthorizationToken(
+          {_i2.PostgreSQLExecutionContext? context,
+          List<_i6.Attribute>? paths,
+          Map<String, dynamic>? data}) =>
+      (super.noSuchMethod(
+              Invocation.method(#listAuthorizationToken, [],
+                  {#context: context, #paths: paths, #data: data}),
+              returnValue: Future<List<_i3.AuthorizationToken>>.value(
+                  <_i3.AuthorizationToken>[]))
+          as _i5.Future<List<_i3.AuthorizationToken>>);
+  @override
+  _i5.Future<_i3.AuthorizationToken?> getAuthorizationToken(
+          {_i2.PostgreSQLExecutionContext? context,
+          Map<String, dynamic>? data,
+          List<_i6.Attribute>? paths}) =>
+      (super.noSuchMethod(
+              Invocation.method(#getAuthorizationToken, [],
+                  {#context: context, #data: data, #paths: paths}),
+              returnValue: Future<_i3.AuthorizationToken?>.value())
+          as _i5.Future<_i3.AuthorizationToken?>);
+  @override
+  _i5.Future<bool> deleteAuthorizationToken(
+          {_i2.PostgreSQLExecutionContext? context,
+          Map<String, dynamic>? data}) =>
+      (super.noSuchMethod(
+          Invocation.method(
+              #deleteAuthorizationToken, [], {#context: context, #data: data}),
+          returnValue: Future<bool>.value(false)) as _i5.Future<bool>);
+  @override
+  String toString() => super.toString();
+}
+
+/// A class which mocks [JsonWebToken].
+///
+/// See the documentation for Mockito's code generation for more information.
+class MockJsonWebToken extends _i1.Mock implements _i9.JsonWebToken {
+  MockJsonWebToken() {
+    _i1.throwOnMissingStub(this);
+  }
+
+  @override
+  String generateAuthorizationToken({Map<String, dynamic>? payload}) =>
+      (super.noSuchMethod(
+          Invocation.method(
+              #generateAuthorizationToken, [], {#payload: payload}),
+          returnValue: '') as String);
+  @override
+  String generateRefreshToken({Map<String, dynamic>? payload}) =>
+      (super.noSuchMethod(
+          Invocation.method(#generateRefreshToken, [], {#payload: payload}),
+          returnValue: '') as String);
+  @override
+  Map<String, dynamic> verify(String? token, String? tokenName) =>
+      (super.noSuchMethod(Invocation.method(#verify, [token, tokenName]),
+          returnValue: <String, dynamic>{}) as Map<String, dynamic>);
   @override
   String toString() => super.toString();
 }
